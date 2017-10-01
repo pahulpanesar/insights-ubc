faf49bd4580cb6fe547caa700d13ccb5629a017c
Pahul <pahulpn@gmail.com>
2017-10-01 16:15:27 -0700

    got data outputting correctly

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -4,22 +4,39 @@
 import {IInsightFacade, InsightResponse} from "./IInsightFacade";
 import Log from "../Util";
 import * as JSZIP from "jszip";
+import {escape} from "querystring";
 
 export default class InsightFacade implements IInsightFacade {
 
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
     }
-
     addDataset(id: string, content: string): Promise<InsightResponse> {
         return new Promise(function (fulfill, reject) {
+            var files: any;
             var zip = new JSZIP();
+            var pArr: Array<Promise<any>> = [];
             zip.loadAsync(content, {base64: true}).then(function (zip) {
-                console.log(zip.files);
-                fulfill(null);
-            }).catch(function (zip) {
-                reject("Error: Not base64");
-            });
+                files = zip.files;
+                Object.keys(files).forEach(function(filename) {
+                    let file: JSZipObject = files[filename];
+                    pArr.push(
+                    file.async('string').then(function(fileData) {
+                        console.log(fileData);
+                    }));
+                })
+            })
+                .then(function (next) {
+                    Promise.all(pArr).then(function(ful) {
+                        fulfill(null);
+                    })
+                        .catch(function(err) {
+                            reject("Promise all");
+                    })
+                })
+                .catch(function (err) {
+                    reject("Error: Not base64");
+                })
         });
     }
 
