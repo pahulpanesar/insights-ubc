1e268024b5fc71ca23dfa4bece741fcf902d1542
Pahul <pahulpn@gmail.com>
2017-10-16 17:49:04 -0700

    another one

--- src/controller/nodes/InputStringNode.ts
+++ src/controller/nodes/InputStringNode.ts
@@ -13,17 +13,14 @@ export default class InputStringNode extends _Node{
         var s = this.getAndCheckToken("[^*]", true);
         this.inputString = s;
         let temp: string = this.inputString;
-        if(this.inputString.charAt(0) === '*' && this.inputString.charAt(this.inputString.length - 1) === '*' && this.inputString.length > 2){
+        if(this.inputString.charAt(0) === '*' && this.inputString.charAt(this.inputString.length - 1) === '*'){
             this.inputString = '^' + this.inputString.substring(1, this.inputString.length - 1) + '$';
         }
         else if(this.inputString.charAt(0) === '*' && this.inputString.length > 1) {
             this.inputString = this.inputString.substring(1) + '$';
         }
-        else if(this.inputString.charAt(this.inputString.length - 1 ) === '*' && this.inputString.length > 1) {
+        else if(this.inputString.charAt(this.inputString.length - 1 ) === '*') {
             this.inputString = '^' + this.inputString.substring(0, this.inputString.length - 1) ;
-        }
-        else if(this.inputString.indexOf('*') !== -1) {
-            throw error("Incorrect wildcard positioning")
         }
         else {
             this.inputString = '^' + this.inputString + '$';
